379542dbf2bb918affc63c46145906d1e0741b45
ebonillo <148749439+ebonillo@users.noreply.github.com>
2024-04-11 11:31:26 +0000

    Ejercicios día 4

--- src/app/exercises/components/hello-world/hello-world.component.ts
+++ src/app/exercises/components/hello-world/hello-world.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-hello-world',
   standalone: true,
-  template: ``,
+  template: `<h1 class="hello">Hello world</h1>`,
+  styles: `
+    .hello {
+      color: var(--primary-color);
+    }
+  `,
 })
 export class HelloWorldComponent {}
--- src/app/exercises/typescript/generics/exercise/generics.ts
+++ src/app/exercises/typescript/generics/exercise/generics.ts
@@ -1,1 +1,14 @@
-// @ts-nocheck
+// // @ts-nocheck
+
+// ej 1
+function identity<T>(arg: T): T {
+  return arg
+}
+
+// ej 2
+class Generic<R, T = string> {
+  foo?: T
+  bar(x: T): R {
+    return x as unknown as R
+  }
+}
