2229aa5031d301e89749201192e57f3de45f03d8
Esperanza <148749439+ebonillo@users.noreply.github.com>
2024-04-15 12:40:11 +0000

    Ejercicios 15/02/24

--- src/app/exercises/components/forms/forms.component.ts
+++ src/app/exercises/components/forms/forms.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core'
-import { ReactiveFormsModule } from '@angular/forms'
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-forms',
@@ -8,4 +8,23 @@ import { ReactiveFormsModule } from '@angular/forms'
   styleUrl: './forms.component.css',
   imports: [ReactiveFormsModule],
 })
-export class FormsComponent {}
+export class FormsComponent {
+  errorMessage = ''
+  habits: { habit: string; date: Date }[] = []
+  habitForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+  })
+  handleSubmit() {
+    if (this.habitForm.status === 'INVALID') {
+      this.errorMessage = 'El nombre del hábito debe tener, al menos, 2 letras'
+      return
+    }
+    this.errorMessage = ''
+    const newHabit: { habit: string; date: Date } = {
+      habit: this.habitForm.value.name!,
+      date: new Date(),
+    }
+    this.habits = [...this.habits, newHabit]
+    this.habitForm.reset()
+  }
+}
