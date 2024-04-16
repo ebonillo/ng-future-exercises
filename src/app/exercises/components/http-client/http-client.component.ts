4756b33a60146f623a806b7f3d56bf3254d2fc4f
ebonillo <148749439+ebonillo@users.noreply.github.com>
2024-04-16 11:11:46 +0000

    Ejercicio http-client sin terminar

--- src/app/exercises/components/http-client/http-client.component.ts
+++ src/app/exercises/components/http-client/http-client.component.ts
@@ -1,8 +1,50 @@
+import { HttpClient } from '@angular/common/http'
 import { Component } from '@angular/core'
+import { lastValueFrom } from 'rxjs'
+
+interface Post {
+  id: number
+  title: string
+  body: string
+  userId: number
+}
+
+type CreatePost = Omit<Post, 'id'>
 
 @Component({
   selector: 'app-http-client',
   standalone: true,
-  template: ``,
+  template: ` <button (click)="getPosts()">Get posts</button>
+    <button (click)="createPost()">Create</button>
+    <button (click)="getError()">Error</button>
+    {{ createdPost?.title }}
+    <div>
+      @for (post of posts; track post.id) {
+        <p>{{ post.title }}</p>
+      }
+    </div>`,
 })
-export class HttpClientComponent {}
+export class HttpClientComponent {
+  posts: Post[] = []
+  createdPost: Post | null = null
+
+  constructor(private readonly httpClient: HttpClient) {}
+
+  async getPosts() {
+    const result = await lastValueFrom(this.httpClient.get<Post[]>('posts'))
+    this.posts = result
+  }
+
+  getError() {
+    lastValueFrom(this.httpClient.get<Post[]>('non_existent_url'))
+  }
+
+  async createPost() {
+    const body: CreatePost = {
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    }
+    this.createdPost = await lastValueFrom(this.httpClient.post<Post>('posts', body))
+  }
+}
