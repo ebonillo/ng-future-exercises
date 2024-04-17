93bd8d0c91ebc77ff9d3214b39caeec0cca899b3
Esperanza <148749439+ebonillo@users.noreply.github.com>
2024-04-17 11:27:48 +0000

    Sin terminar

--- src/app/exercises/components/router/routes.ts
+++ src/app/exercises/components/router/routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router'
 import { SolutionRedirectPageComponent } from './_solution/solution-redirect-page/solution-redirect-page.component'
 import { RouterPageNotFoundSolutionComponent } from './_solution/not-fount-page/solution-router-page-not-found.component'
 import { SolutionRouteParamsPageComponent } from './_solution/solution-route-params-page/solution-route-params-page.component'
+import { Component1Component } from './exercise/component1/component1.component'
 
 export const routesOfRouterExercise: Routes = [
   {
@@ -25,6 +26,12 @@ export const routesOfRouterExercise: Routes = [
   },
   {
     path: 'exercise',
-    children: [],
+    children: [
+      {
+        title: 'Component 1',
+        path: 'c1',
+        component: Component1Component,
+      },
+    ],
   },
 ]
