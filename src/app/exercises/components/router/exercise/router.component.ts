2e970ecd6468ba01f0cf415193510c889c9fe2cc
Esperanza <148749439+ebonillo@users.noreply.github.com>
2024-04-17 11:19:08 +0000

    Sin terminar

--- src/app/exercises/components/router/exercise/router.component.ts
+++ src/app/exercises/components/router/exercise/router.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-router',
   templateUrl: './router.component.html',
   styleUrl: './router.component.css',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
 })
 export class RouterComponent {}
